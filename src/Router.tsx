9003537e4c55322e8a837565fccac279cc8299fc
Betiza Barreira <be.penques@hotmail.com>
2024-06-11 11:36:41 -0300

    arquivos inicias + header + react router

---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DeafultLayout'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+      <Route />
+    </Routes>
+  )
+}
+
+/* path - qual o endereço que apessoa estara acessando
+element - qual componente vai carregar nesse caminho do path */
