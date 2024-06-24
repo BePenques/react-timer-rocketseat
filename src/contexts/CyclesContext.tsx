5a613d8ef1ab15f617f99715b1330c673d003836
Betiza Barreira <be.penques@hotmail.com>
2024-06-24 10:32:42 -0300

    separando actions

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,17 +1,15 @@
 import { ReactNode, createContext, useReducer, useState } from 'react'
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
+import {
+  ActionTypes,
+  addNewCycleAction,
+  interruptCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from './actions'
 
 interface CreateCycleData {
   task: string
   minutesAmount: number
-}
-
-interface Cycle {
-  id: string
-  task: string
-  minutesAmount: number
-  startDate: Date
-  interruptDate?: Date
-  finishedDate?: Date
 }
 
 interface CyclesContextType {
@@ -30,11 +28,6 @@ interface CyclesContextProviderProps {
   children: ReactNode // qualquer html/jsx válido
 }
 
-interface CyclesState {
-  cycles: Cycle[]
-  activeCycleId: string | null
-}
-
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
@@ -42,55 +35,10 @@ export function CyclesContextProvider({
   // 1º param -> valor atual de cycles + ação que o usuario esta querendo realizar
   // 2º -> estado inicial -> []
   // setCycles passa a ser o metodo para disparar a acao,não mais o metodo de alteração - novo nome dispatch
-  const [cyclesState, dispatch] = useReducer(
-    (state: CyclesState, action: any) => {
-      switch (action.type) {
-        case 'ADD_NEW_CYCLE':
-          return {
-            ...state,
-            cycles: [...state.cycles, action.payload.newCycle],
-            activeCycleId: action.payload.newCycle.id,
-          }
-        case 'INTERRUPT_CURRENT_CYCLE':
-          return {
-            ...state,
-            cycles: state.cycles.map((cycle) => {
-              // if (cycle.id === state.activeCycleId) {
-              if (cycle.id === action.payload.activeCycleId) {
-                return {
-                  ...cycle,
-                  interruptDate: new Date(),
-                }
-              } else {
-                return cycle
-              }
-            }),
-            activeCycleId: null,
-          }
-        case 'MARK_CURRENT_CYCLE_AS_FINISHED':
-          return {
-            ...state,
-            cycles: state.cycles.map((cycle) => {
-              if (cycle.id === action.payload.activeCycleId) {
-                return {
-                  ...cycle,
-                  finishedDate: new Date(),
-                }
-              } else {
-                return cycle
-              }
-            }),
-            activeCycleId: null,
-          }
-        default:
-          return state
-      }
-    },
-    {
-      cycles: [],
-      activeCycleId: null,
-    },
-  )
+  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
+    cycles: [],
+    activeCycleId: null,
+  })
 
   const { cycles, activeCycleId } = cyclesState
   // const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
@@ -103,13 +51,7 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch({
-      type: 'MARK_CURRENT_CYCLE_AS_FINISHED', // qual a ação
-      payload: {
-        // dados para serem usados na ação
-        activeCycleId,
-      },
-    })
+    dispatch(markCurrentCycleAsFinishedAction())
     // setCycles((state) =>
     //   state.map((el) => {
     //     if (activeCycleId === el.id) {
@@ -128,13 +70,7 @@ export function CyclesContextProvider({
       startDate: new Date(),
     }
 
-    dispatch({
-      type: 'ADD_NEW_CYCLE', // qual a ação
-      payload: {
-        // dados para serem usados na ação
-        newCycle,
-      },
-    })
+    dispatch(addNewCycleAction(newCycle))
     // setCycles((state) => [...state, newCycle])
     // setActiveCycleId(newCycle.id)
     setAmountSecondsPassed(0)
@@ -151,13 +87,7 @@ export function CyclesContextProvider({
     //   }),
     // )
 
-    dispatch({
-      type: 'INTERRUPT_CURRENT_CYCLE', // qual a ação
-      payload: {
-        // dados para serem usados na ação
-        activeCycleId,
-      },
-    })
+    dispatch(interruptCycleAction(activeCycleId))
 
     // setActiveCycleId(null)
     setAmountSecondsPassed(0)
